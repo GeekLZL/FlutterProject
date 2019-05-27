58052d50463371bea65602d1f90c19783a882935
lizhenlei <379321583@qq.com>
2019-05-27 17:46:10 +0800

    first commit

---
+++ android/app/src/main/java/com/demo/my_flutter_douguo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.demo.my_flutter_douguo;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
